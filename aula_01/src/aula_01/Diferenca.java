37a58b8677712932c42113cb96ea5bfa2d77b5fa
Danilo Moraes <danilot.moraes@gmail.com>
2023-02-08 16:17:22 -0300

    Mudança na pasta Diferença para float

--- aula_01/src/aula_01/Diferenca.java
+++ aula_01/src/aula_01/Diferenca.java
@@ -6,18 +6,18 @@ public class Diferenca {
 		public static void main(String[]args) {
 			Scanner leia = new Scanner(System.in);
 			
-			int n1, n2, n3, n4, dif;
-			System.out.println("Digire o Primeiro número: ");
-			n1 = leia.nextInt();
+			float n1, n2, n3, n4, dif;
+			System.out.printf("Digire o Primeiro número: ");
+			n1 = leia.nextFloat();
 			System.out.println("Digire o Segundo número: ");
-			n2 = leia.nextInt();
+			n2 = leia.nextFloat();
 			System.out.println("Digire o Terceiro número: ");
-			n3 = leia.nextInt();
+			n3 = leia.nextFloat();
 			System.out.println("Digire o Quarto número: ");
-			n4 = leia.nextInt();
+			n4 = leia.nextFloat();
 			
 			dif = (n1 * n2) - (n3 * n4);
 			
-			System.out.printf("A difetença é: %d", dif);
+			System.out.printf("A difetença é: %.0f", dif);
 		}
 }
