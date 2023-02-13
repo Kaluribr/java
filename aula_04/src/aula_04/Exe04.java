ae398b678033b87e6864515d26d411d6bd5fcc99
Danilo Moraes <danilot.moraes@gmail.com>
2023-02-13 16:13:48 -0300

    ex 04

--- aula_04/src/aula_04/Exe04.java
+++ aula_04/src/aula_04/Exe04.java
@@ -1,10 +1,44 @@
 package aula_04;
 
+import java.util.Scanner;
+
 public class Exe04 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner leia = new Scanner(System.in);
+		
+		float matriz[][] = new float[3][4];
+		float media[] = new float[3];
+		float soma = 0.0f;
+		
+		for(int linha = 0; linha < matriz.length; linha ++) {
+			
+			for(int coluna = 0; coluna < matriz[linha].length; coluna++) {
+				
+				System.out.println("Digite a nota: ");
+				matriz[linha][coluna] = leia.nextFloat();
+				
+			}
+		}
+		
+		for(int linha = 0; linha < matriz.length; linha ++) {
+			
+			for(int coluna = 0; coluna < matriz[linha].length; coluna++) {
+				
+				soma += matriz[linha][coluna];
+				
+			}
+			
+			media[linha] = soma / matriz[linha].length;
+			soma = 0.0f;
+			
+		}
+		
+		for(var meidas : media)
+			System.out.println(media);
+		
+		leia.close();
 	}
 
 }
