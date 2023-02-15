487775d81cad604445167f8ccee3d8900510a78c
Danilo Moraes <danilot.moraes@gmail.com>
2023-02-15 20:10:12 -0300

    Estava muito dificil

--- aula_05/src/aula_05/Exe02.java
+++ aula_05/src/aula_05/Exe02.java
@@ -8,23 +8,30 @@ public class Exe02 {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		
-		ArrayList<Double> numeros = new ArrayList<Double>();
+		ArrayList<Integer> numeros = new ArrayList<Integer>();
+		int num = 0;
 		
-		double numero = 0.0;
+		numeros.add(2);
+		numeros.add(5);
+		numeros.add(1);
+		numeros.add(3);
+		numeros.add(4);
+		numeros.add(9);
+		numeros.add(7);
+		numeros.add(8);
+		numeros.add(10);
+		numeros.add(6);
 		
-		numeros.add(2.0);
-		numeros.add(5.0);
-		numeros.add(1.0);
-		numeros.add(3.0);
-		numeros.add(4.0);
-		numeros.add(9.0);
-		numeros.add(7.0);
-		numeros.add(8.0);
-		numeros.add(10.0);
-		numeros.add(6.0);
+		if (num < 10) {
+			System.out.println("Digite o número que você deseja encontrar: ");
+			num = leia.nextInt();
+			System.out.println("O número " + num + " está localizado na posição: " + numeros.indexOf(num));
+		}else
+			System.out.println("O número "+ num +" não foi encontrado!");
 		
+	
 		
-		
+		leia.close();
 	}
 
 }
